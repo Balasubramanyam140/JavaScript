b21d22b565df2b61b086c3daa37778ff28ca8386
Balasubramanyam140 <108879990+Balasubramanyam140@users.noreply.github.com>
2024-10-07 19:38:52 +0530

    Create 07. Undefined & NULL value.js

---
+++ 07. Undefined & NULL value.js
@@ -0,0 +1,12 @@
+          console.clear()
+          
+          var mVar //mVar holds no value
+          console.log(mVar)
+          
+          mVar = null //mVar holds the vale null 
+          console.log(mVar)
+          
+
+OUTPUT:
+          undefined
+          null

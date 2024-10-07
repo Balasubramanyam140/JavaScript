5feb1b8688dbca3dda8bf21bdeb0ac11eff691e6
Balasubramanyam140 <108879990+Balasubramanyam140@users.noreply.github.com>
2024-10-07 18:10:46 +0530

    Create 03. Rules for Var names.js

---
+++ 03. Rules for Var names.js
@@ -0,0 +1,22 @@
+1) Rules for variable names
+
+              //Camel Case (recommended for JS)
+              var firstNum;
+              
+              //Pascal case
+              var FirstNum;
+              
+              //Snake case
+              var first_num;
+              
+              //Rules for naming JS variables
+              // - Names can contain letters, digits, _ and $
+              var mysub1, math$;
+              // - Names cannot start with a digit
+              var maths; //gives an error
+              // - Names can start with an _ aor $
+              var _sub, $hi;
+              // - Names are case senstitive
+              var x, X;
+              // - Names cannot be reserved keywords 
+              var varName;

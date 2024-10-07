690240d41ef0c6d3a6b288ec7d37dafdea927fe7
Balasubramanyam140 <108879990+Balasubramanyam140@users.noreply.github.com>
2024-10-07 18:04:21 +0530

    Create 02. console.clear().js

---
+++ 02. console.clear().js
@@ -0,0 +1,13 @@
+1) To remove old outputs and show only new output in console
+                
+                console.clear();
+                
+                var fnum = 10;
+                console.log(fnum);
+                
+                var snum = 15;
+                console.log(snum);
+
+Output :
+            10
+            15

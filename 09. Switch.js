afdd844c976a95399cee00aceb8b1a3a0bf2cc21
Balasubramanyam140 <108879990+Balasubramanyam140@users.noreply.github.com>
2024-10-07 21:30:53 +0530

    Create 09. Switch.js

---
+++ 09. Switch.js
@@ -0,0 +1,43 @@
+console.clear()
+
+var currentDay = 'Mon'
+
+if(currentDay ==='Mon'){
+    console.log('Timings: 10:00 - 06:00')
+}else if(currentDay ==='Tue'){
+    console.log('Timings: 09:00 - 05:00')
+}else if(currentDay ==='Wed'){
+    console.log('Timings: 09:00 - 05:00')
+}else if(currentDay ==='Thur'){
+    console.log('Timings: 09:00 - 05:00')
+}else if(currentDay ==='Fri'){
+    console.log('Timings: 09:00 - 05:00')
+}else if(currentDay ==='Sat'){
+    console.log('Timings: 09:00 - 05:00')
+}else{
+    console.log('Timings: 09:00 - 05:00')
+}
+switch(currentDay){
+    case 'Mon':
+        console.log('Timingfs: 10:00 - 06:00')
+        break;
+    case 'Tue':
+        console.log('Timings: 09:00 - 05:00')
+        break;
+    case 'Wed':
+        console.log('Timingfs: 10:00 - 06:00')
+        break;
+    case 'Thur':
+        console.log('Timings: 09:00 - 05:00')
+        break;
+    case 'Fri':
+        console.log('Timingfs: 10:00 - 06:00')
+        break;
+    case 'Sat':
+        console.log('Timings: 09:00 - 05:00')
+        break;   
+}
+
+OUTPUT :
+          Timings: 10:00 - 06:00
+          Timingfs: 10:00 - 06:00

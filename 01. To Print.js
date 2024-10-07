f4429797da4458252ceb1798d7977459dab3c7ce
Balasubramanyam140 <108879990+Balasubramanyam140@users.noreply.github.com>
2024-10-07 17:52:08 +0530

    01. To Print.js

---
+++ 01. To Print.js
@@ -0,0 +1,15 @@
+1. How to priunt in js
+              
+              var num1 = 2;
+              var num2 = 4;
+              var num3 = 6;
+              var sum = num1 + num2 + num3;
+              var firstName = "Balu";
+              
+              console.log(firstName)
+              console.log(sum)
+              
+Output:
+          
+          Balu
+          12
